cbf78121939ef5e551017514c51d112755cafd1a
dev.epi <nourhene.epi@gmail.com>
2026-04-30 10:38:48 +0100

    users crud

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -10,4 +10,54 @@ const create = async(req , res)=>{
     }
 }
 
-module.exports = {create}
+const getAll = (req , res)=>{
+    
+    UserModel.find().then((users)=>{
+         res.send(users)
+        
+    }).catch(err=>{
+        res.status(430).send(err)
+    })
+}
+
+const getByRole = async(req , res)=>{
+    
+    try{
+        let {role} = req.params
+        let users = await UserModel.find({role : role})
+        res.send(users)
+    }catch(err){
+        res.status(430).send(err)
+    }
+}
+const getById = async(req , res)=>{
+    
+    try{
+        let {id} = req.params
+        let user = await UserModel.findOne({_id : id})
+        res.send(user)
+    }catch(err){
+        res.status(430).send(err)
+    }
+}
+const update = async(req , res)=>{
+    
+    try{
+     let user=  await UserModel.updateOne({_id : req.params.id} , req.body)
+     res.send(user)
+    
+    }catch(err){
+        res.status(430).send(err)
+    }
+}
+const remove = async(req , res)=>{
+    
+    try{
+     let user=  await UserModel.deleteOne({_id : req.params.id})
+     res.send(user)
+    
+    }catch(err){
+        res.status(430).send(err)
+    }
+}
+module.exports = {create, getAll , getByRole , getById , update , remove}
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,11 @@
-const { create } = require('../controllers/users.controller');
+const { create, getAll, getByRole, getById, update, remove } = require('../controllers/users.controller');
 
 const router = require('express').Router();
 
 router.post('/create' , create)
-
+router.get('/' , getAll)
+router.get('/role/:role' , getByRole)
+router.get('/:id' , getById)
+router.put('/:id' , update)
+router.delete('/:id' , remove)
 module.exports = router
